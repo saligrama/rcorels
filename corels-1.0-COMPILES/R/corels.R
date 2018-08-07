cf977122316b3cc725eb15a5d3ce50928e8d5a13
Aditya Saligrama <saligrama427@gmail.com>
2018-08-07 18:41:34 -0400

    cleanup the repo a bit

--- corels-1.0-COMPILES/R/corels.R
+++
@@ -1,4 +0,0 @@
-corels <- function(data_fname,label_fname,meta_fname,curiosity_policy=2,max_nodes=10000, regularization=0.01, verbosity=list("progress"), map_type=2, ablation=0, calculate_size=FALSE, latex_out=FALSE) {
-    params_list <- list(curiosity_policy, max_nodes, regularization, verbosity, map_type, ablation, calculate_size, latex_out)
-    .Call('_run_corels', PACKAGE='corels', 0, 0, params_list, data_fname, label_fname, meta_fname)
-}
